baf31661248793750b8ad3428469e5927bdf8c21
Maksim Nikolaev <nnnata24@mail.ru>
2023-05-17 12:58:32 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://9ceac056-73bb-4469-b86f-cbbaa7269b85.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
